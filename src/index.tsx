f38754ccea3a28270388d402a0c088833f5c2854
Steve McKeogh <steve.mckeogh@gmail.com>
2018-12-27 11:54:47 +0000

    chore(lint): Added tslint

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Home from './pages/Home'
-import * as serviceWorker from './serviceWorker'
 import './scss/app.scss'
+import * as serviceWorker from './serviceWorker'
 
 ReactDOM.render(<Home />, document.getElementById('root'))
 
